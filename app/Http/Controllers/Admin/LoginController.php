b3d98e8712945ae5bc420fe09663b937e796b20a
leekachung <ljc970910@163.com>
2018-10-02 17:38:42 +0800

    Fixed: 修改部分注释

--- app/Http/Controllers/Admin/LoginController.php
+++ app/Http/Controllers/Admin/LoginController.php
@@ -13,12 +13,20 @@ use Validator;
 
 class LoginController extends Controller
 {
-    
+ 
+    /**
+     * 后台登录模块
+     * @author leekachung <[leekachung17@gmail.com]>
+     * @DateTime         2018-10-02T01:18:58+0800
+     * @return
+     */
     public function index(Request $request)
     {
+        //判断请求方式
     	if ($request->isMethod('get')) {
             return view('admin.login');
         } elseif ($request->isMethod('post')) {
+            //数据验证
             $validator = Validator::make($request->all(), [
                     'username' => 'required|min:2|max:16',
                     'password' => 'required'
@@ -30,10 +38,14 @@ class LoginController extends Controller
             		->withInput();
             }
             
+            //用户认证
             if (Auth::attempt([
             	'username' => $request->username,
             	'password' => $request->password,
             ])) {
+                session([
+                    'status' => "欢迎回来, {$request->username}"
+                ]);
             	return redirect(route('admin.index.index'));
             } else {
  				return back()->withErrors('用户名或密码错误');
